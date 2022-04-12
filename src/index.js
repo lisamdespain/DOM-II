4963913a623695496b923af08834ee42790a0800
Lisa DeSpain <lisamdespain@gmail.com>
2022-04-12 17:02:38 -0500

    working on task 8

--- src/index.js
+++ src/index.js
@@ -35,16 +35,50 @@ function fourKey(evt){
 }
 document.addEventListener('keydown', fourKey);
 
-//Task 4: WHEEL: When the mouse is over the header image and use the mouse wheel, it will become 90% smaller & not scroll the website. Will need to not have the mouse over the image to scroll further down. https://developer.mozilla.org/en-US/docs/Web/API/Element/wheel_event
+//Task 4: WHEEL: When the mouse is over the header image and use the mouse wheel, it will become 110% bigger & not scroll the website. Will need to not have the mouse over the image to scroll further down. https://developer.mozilla.org/en-US/docs/Web/API/Element/wheel_event
 
 const bigImage = document.querySelector(".intro img")
 function zoom(e){
     e.preventDefault();
     // let scale += e.deltaY * -0.01;
     // let scale = Math.min(Math.max(.125, scale), 4);
-    let scale = .9;
+    let scale = 1.1;
     bigImage.style.transform = `scale(${scale})`;
 }
 
 bigImage.addEventListener('wheel', zoom);
+
+//Task 5: LOAD: when the window loads, give an alert. Wishing I could add confetti. Confetti: https://css-tricks.com/write-code-get-confetti/
+
+window.addEventListener('load', () => {
+    alert('The page is fully loaded');
+  });
+
+//Task 6: FOCUS: Make the first a tag in the header italic on focus.
+const links = document.querySelector('a');
+function makeLinkItalic(){
+    links.style.fontStyle = "italic";
+    links.style.color = "red";
+    links.style.fontWeight = 'bold';
+}
+links.addEventListener('focus', makeLinkItalic);
+
+// Task 7: RESIZE: When the user resizes the window, insert the new dimensions into the body. Very weird/interesting behavior - how to get it to report back ONLY when it's done resizing...?
+
+function windowSize(){
+    const resizeMessage = document.createElement("p");
+    document.body.appendChild(resizeMessage);
+    resizeMessage.textContent = `The new window size is ${window.innerHeight} high and ${window.innerWidth} wide.`;
+}
+window.addEventListener('resize', windowSize);
+
+// Task 8: SELECT: Select some text, do something...
+
+document.querySelectorAll('p').forEach(text => text.addEventListener('select', doThis => {
+    doThis.target.style.background = 'yellow';
+    setTimeout(function() {
+        doThis.target.style.background = "";
+      }, 600);
+    }
+));
 
